62c4ed86732410b4b2e649048ae8fdf2f1918947
aidarkhanovaalima <158269494+aidarkhanovaalima@users.noreply.github.com>
2024-01-31 15:20:52 +0600

    Aidarkhanova Alima Lab2

---
+++ Aidarkhanova Alima 2 lab.py
@@ -0,0 +1,96 @@
+PYTON Syntax
+#Exercise 1
+print("Hello World")
+
+#Exercise 2
+if 5 > 2:
+    print("YES")
+
+PYTHON Comments
+#Exercise 1
+#This is a comment
+
+#Exercise 2
+""""
+This is a comment
+written in 
+more than just one line
+"""
+
+PYTHON Variables
+#Exercise1
+carname = "Volvo"
+
+#Exercise 2
+x = 50 
+
+#Exercise 3
+x = 5
+y = 10
+print(x+y)
+
+#Exercise 4
+x = 5
+y = 10
+z = x+y
+print(z)
+
+#Exercise 5
+x,y,z = "Orange", "Banana", "Cherry"
+
+#Exercise 6
+x = y = z = "Orange"
+
+#Exercise 7
+def myfunc():
+    global x
+    x = "fantastic"
+
+PYTHON Data Types
+#Exercise 1
+x = 5
+print(type(x))
+int
+
+#Exercise 2
+x = "Hello World"
+print(type(x))
+str
+
+#Exercise 3
+x = 20.5
+print(type(x))
+float
+
+#Exercise 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#Exercise 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#Exercise 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#Exercise 7
+x = True
+print(type(x))
+bool
+
+PYTHON Numbers
+#Exercise 1
+x = 5
+x = float(x)
+
+#Exercise 2
+x = 5.5
+x = int(x)
+
+#Exercise 3
+x = 5
+x = complex(x)
